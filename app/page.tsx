b8b7eb39130d0bbf0bae384dc9bb8598296705e6
Giulio Sá <giuliocaetano@yahoo.com.br>
2024-11-30 13:10:59 -0300

    [LAW] - Prettier configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 // import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+    return (
+        <div className="right-0 w-20 bg-blue-50 font-sans text-blue-50">
+            Hello
+        </div>
+    )
 }
